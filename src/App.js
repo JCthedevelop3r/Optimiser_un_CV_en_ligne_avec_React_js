48d0cc7c9854f732f99acc103c5306abcc189c9a
JIICE <minotjeanclaude60@gmail.com>
2024-04-03 11:47:17 +0100

    Install React router and code the routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from "react";
+import {Route, Routes, Link} from "react-router-dom";
 
+import HomePage from "./components/Pages/HomePage";
+import ServicesPage from "./components/Pages/ServicesPage";
+import ProductionsPage from "./components/Pages/ProductionsPage";
+import BlogPage from "./components/Pages/BlogPage";
+import ContactPage from "./components/Pages/ContactPage";
+import LegalNoticePage from "./components/Pages/LegalNoticePage";
 
 function App() {
   return (
     <div className="App">
       
+      <Routes>
+        <Route path="/" component={<HomePage/>}></Route>
+        <Route path="/ServicesPage" component={<ServicesPage/>}></Route>
+        <Route path="/ProductionsPage" component={<ProductionsPage/>}></Route>
+        <Route path="/BlogPage" component={<BlogPage/>}></Route>
+        <Route path="/ContactPage" component={<ContactPage/>}></Route>
+        <Route path="/LegalNoticePage" component={<LegalNoticePage/>}></Route>
+      </Routes>
     </div>
   );
 }
