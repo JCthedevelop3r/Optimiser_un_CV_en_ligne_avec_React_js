51a0448880f6fe805d026b3a983a5876e6266b89
JIICE <minotjeanclaude60@gmail.com>
2024-04-04 22:41:10 +0100

    Start coding the legal notice page

---
+++ src/components/Pages/LegalNoticePage.jsx
@@ -0,0 +1,9 @@
+
+export default function LegalNoticePage() {
+    return(
+        <div className="legal-notice-page">
+            <h2>mentions légales</h2>
+            {/*Utiliser le composant accordéon de Bootstrap pour coder la suite de la page.*/}
+        </div>
+    );
+}
