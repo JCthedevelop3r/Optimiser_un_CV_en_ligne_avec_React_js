189fe3a58d5ad6da5e81d8f4b319a0f1f02ab493
JIICE <minotjeanclaude60@gmail.com>
2024-04-07 22:14:14 +0100

    Set links of the footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import ServicesPage from "./components/Pages/ServicesPage";
 import ProductionsPage from "./components/Pages/ProductionsPage";
 import BlogPage from "./components/Pages/BlogPage";
 import ContactPage from "./components/Pages/ContactPage";
+import LegalNoticePage from "./components/Pages/LegalNoticePage";
 import HomePage from "./components/Pages/HomePage";
 
 function App() {
@@ -18,7 +20,9 @@ function App() {
           <Route path="/ProductionsPage" element={<ProductionsPage/>}></Route>
           <Route path="/BlogPage" element={<BlogPage/>}></Route>
           <Route path="/ContactPage" element={<ContactPage/>}></Route>
+          <Route path="/LegalNoticePage" element={<LegalNoticePage/>}></Route>
         </Routes>
+        <Footer/>
     </div>
   );
 }
