73f2fc984e95216f92b5e0e0ea4c55723c0f988c
JIICE <minotjeanclaude60@gmail.com>
2024-04-28 13:17:12 +0100

    Make the services page responsive for the tablets

--- src/components/ServicesPage.jsx
+++ src/components/ServicesPage.jsx
@@ -6,7 +6,7 @@ import bannerMobile from '../images/img/banner-mobile.jpg';
 
 function Card({children}) {
     return (
-        <section className="prestations-content">{children}</section>
+        <section>{children}</section>
     );
 }
 
@@ -15,15 +15,13 @@ export default function ServicesPage() {
         <main className='services-page'>
             <section className='text-center d-flex flex-column align-items-center'>
                 <img className="banner-mobile" src={bannerMobile} alt="Rayons lumineux qui se croisent sur fond bleu" />
-                <h2 className='h2-sp text-uppercase mt-5 fs-1'>Mon offre de services</h2>
+                <h2 className='h2-sp text-uppercase fs-1'>Mon offre de services</h2>
                 <p className='text-intro'>Voici les prestations sur lesquelles je peux intervenir.</p>
                 <div className='blue-stroke-sp border border-top border-start-0 border-end-0 border-bottom-0 border-4 border-primary'></div>
             </section>
-            <section className='prestations'>
                 <Card>
                     <Prestations/>
                 </Card>
-            </section>
         </main>
     );
 }
