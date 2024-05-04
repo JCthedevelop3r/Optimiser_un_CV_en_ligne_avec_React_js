a13867426fb9d21ebd86850eb4184b23b26e194f
JIICE <minotjeanclaude60@gmail.com>
2024-05-04 02:55:02 +0100

    refine the John Doe Github profile page

--- src/components/JohnDoeProfile.jsx
+++ src/components/JohnDoeProfile.jsx
@@ -16,19 +16,19 @@ export default function JohnDoeProfile() {
         getUser();
     },[])
     return(
-        <main className='jd-profile-page'>
+        <main className='container jd-profile-page'>
             <section className='presentation-jdpp'>
                 <h1 className='h1-jdpp fs-2'>Github user</h1>
                 <h2 className='h2-jdpp fs-4'>{user.name}</h2>
                 <img className='pdp-jd' src={PhotoDeProfilJohnDoe} alt="dessin de type cartoon représentant un chat bleu." />
             </section>
             <ul className='jd-infos'>
-                <li className='li-jd-infos'>{user.bio}</li>
+                <li className='li-jd-infos text-center'>{user.bio}</li>
                 <li className='li-jd-infos'>Abonnés : {user.followers}</li>
                 <li className='li-jd-infos'>Abonnements : {user.following}</li>
                 <li className='li-jd-infos'>Crée le : {user.created_at}</li>
                 <li className='li-jd-infos'>Modifié le : {user.updated_at}</li>
-                <li className='li-jd-infos'>URL repositories : <a href='https://api.github.com/users/github-john-doe/repos' className='url-repos text-primary'>{user.repos_url}</a></li>
+                <li className='li-jd-infos text-center'>URL repositories : <a href='https://api.github.com/users/github-john-doe/repos' className='url-repos text-primary'>{user.repos_url}</a></li>
             </ul>
         </main>
     )
